b18d07e80a079c5c0ee270ef43c9ee5e822a05c8
Andrew <xyligan2222>
2020-03-25 21:56:59 +0300

    3.3. Разбор первой программы.[#261375]

---
+++ test/ru/job4j/ru/job4j/StudentInfo.java
@@ -0,0 +1,16 @@
+package ru.job4j.ru.job4j;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("1 * 2 = 2");
+        System.out.println("1 * 3 = 3");
+        System.out.println("1 * 4 = 4");
+        System.out.println("1 * 5 = 5");
+        System.out.println("1 * 6 = 6");
+        System.out.println("1 * 7 = 7");
+        System.out.println("1 * 8 = 8");
+        System.out.println("1 * 9 = 9");
+    }
+}
