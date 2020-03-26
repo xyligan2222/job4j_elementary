97bd05a942b505d87215ef2413478e1d3fc4bb6a
xyligan2222 <xyligan2222@yandex.ru>
2020-03-26 11:58:10 +0300

    3. Арифметические операции.[#261367]

---
+++ src/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        int six = 6, five = 5, four = 4, one = 1, two = 2;
+        int sixDivTwo = six/two;
+        int fiveMinusTwo = five-two;
+        int fourTimeTwo = four*two;
+
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
+    }
+}
