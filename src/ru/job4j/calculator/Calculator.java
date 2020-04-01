3e6e4b01a9996c2d939a72fe91446ba9a026744c
Andrew <xyligan2222>
2020-04-01 21:12:24 +0300

    5.6. Простые числа.[#261398]  начал цикл с 2 включительно до переменной finish

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,12 +1,19 @@
 package ru.job4j.calculator;
+/**
+ * Package for calculate task.
+ *
+ * @author Andrey Shirokov (xyligan2222@yandex.ru)
+ * @version Idea 2019.3.4
+ * @since 01.04.2020
+ */
 
 public class Calculator {
 
     public static void main(String[] args) {
         int six = 6, five = 5, four = 4, one = 1, two = 2;
-        int sixDivTwo = six/two;
-        int fiveMinusTwo = five-two;
-        int fourTimeTwo = four*two;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
 
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
