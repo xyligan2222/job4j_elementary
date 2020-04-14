3ba145b2f6f3cf1289ee544f5808d26857d5abb8
xyligan2222 <xyligan2222@yandex.ru>
2020-04-14 11:15:37 +0300

    2.2. Рефакторинг - Площадь треугольника.[#261483]

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -1,9 +1,35 @@
 package ru.job4j.condition;
 
 public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
 
-    public static boolean exist(double ab, double ac, double bc) {
-        boolean result = (ab + ac) > bc && (ab + bc) > ac && (ac + bc) > ab;
-        return result;
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    private boolean exist(double a, double c, double b) {
+
+        return (a + b) > c && (a + c) > b && (b + c) > a;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return rsl;
     }
 }
--- src/ru/job4j/condition/TriangleTest.java
+++ src/ru/job4j/condition/TriangleTest.java
@@ -1,13 +1,20 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
-import static org.hamcrest.core.Is.is;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
 import static org.junit.Assert.assertThat;
+
 
 public class TriangleTest {
     @Test
-    public void whenExist() {
-        boolean result = Triangle.exist(3.0, 2.0, 2.0);
-        assertThat(result, is(true));
+    public void whenAreaSetThreePointsThenTriangleArea() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(2, 0);
+        Triangle tr = new Triangle(a, b, c);
+        double result = tr.area();
+        double expected = 2D;
+        assertThat(result, closeTo(expected, 0.1));
     }
 }
